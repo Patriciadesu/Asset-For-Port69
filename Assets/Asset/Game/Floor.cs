5998ec2e69c12865aef1261926be16746b011294
Patriciadesu <118905032+Patriciadesu@users.noreply.github.com>
2025-04-04 21:47:31 +0700

    Update Floor.cs

--- Assets/Asset/Game/Floor.cs
+++ Assets/Asset/Game/Floor.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 public class Floor : MonoBehaviour
 {
     [SerializeReference] public List<FloorEffect> objectives = new List<FloorEffect>();
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        foreach (FloorEffect obj in objectives)
+        {
+            obj.CollisionEnter(collision);
+        }
+    }
+    public void OnCollisionStay(Collision collision)
+    {
+        foreach (FloorEffect obj in objectives)
+        {
+            obj.CollisionStay(collision);
+        }
+    }
+
 }
