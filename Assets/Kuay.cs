c0b5e44531807bbb404b0b355c5150204ce52c34
Patriciadesu <118905032+Patriciadesu@users.noreply.github.com>
2025-10-30 22:26:35 +0700

    update

---
+++ Assets/Kuay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Kuay : ObjectEffect
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float playerHealth = Player.Instance.Stat.maxhealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
